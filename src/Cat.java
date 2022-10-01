5ec16fa2432cc2488478a05b8a002c321b209a44
Kyle Smart <mckylssir@gmail.com>
2022-09-30 20:23:07 -0600

    Third commit, changed cats dimensions

--- src/Cat.java
+++ src/Cat.java
@@ -7,18 +7,18 @@ public class Cat {
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 100;
 	// eyes will be about 1/4 from top of head and 1/4 from left
-	private static final int EYE_Y = HEAD_DIMENSION/4;
-	private static final int EYE_X = HEAD_DIMENSION/4;
+	private static final int EYE_Y = HEAD_DIMENSION*4;
+	private static final int EYE_X = HEAD_DIMENSION*4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 20;
-	private static final int EYE_WIDTH = 10;
+	private static final int EYE_HEIGHT = 22;
+	private static final int EYE_WIDTH = 13;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
+	private static final int MOUTH_HEIGHT = 14;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 * 14;
+	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 9;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
